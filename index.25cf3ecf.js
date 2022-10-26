2d76b799e9632090e8cd3f16ede1886348db0de6
SeredaAndrey <102906498+SeredaAndrey@users.noreply.github.com>
2022-10-26 07:24:25 +0000

    Deploying to gh-pages from @ SeredaAndrey/goit-js-hw-11@6a1b112050aff78e3c544b8dcc5c87cd90c6de04 🚀

---
+++ index.25cf3ecf.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector(".gallery");
+//# sourceMappingURL=index.25cf3ecf.js.map
---
+++ index.ed73ee5d.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector(".gallery");
+//# sourceMappingURL=index.ed73ee5d.js.map
