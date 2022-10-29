98e31bdf7f8deac9421b9d76b88c123c9317d2e7
Andrii Sereda <a.a.sereda@ukr.net>
2022-10-29 18:54:34 +0300

    rvnerv

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import refs from './js/refs.js';
 import RestGalleryService from './js/fetchgallary';
 
 import Notiflix from 'notiflix';
+
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import photoCard from './templates/photocard.hbs';
@@ -12,15 +12,6 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const restGalleryService = new RestGalleryService();
-
-let lightbox = new SimpleLightbox('.photo-card a', {
-  captions: true, //true	bool	show captions if availabled or not
-  captionSelector: 'img', //'img'	string or function	set the element where the caption is. Set it to "self" for the A-Tag itself or use a callback which returns the element
-  captionType: 'attr', //'attr'	string	how to get the caption. You can choose between attr, data or text
-  captionsData: 'alt', //title	string	get the caption from given attribute
-  captionPosition: 'bottom', //'bottom'	string	the position of the caption. Options are top, bottom or outside (note that outside can be outside the visible viewport!)
-  captionDelay: 250, //int	adds a delay before the caption shows (in ms)
-});
 
 // слушатель событий на ввод в поле поиска
 refs.searchForm.addEventListener(
@@ -45,9 +36,19 @@ refs.gallery.addEventListener('click', event => {
   event.preventDefault();
 });
 
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true, //true	bool	show captions if availabled or not
+  captionSelector: 'img', //'img'	string or function	set the element where the caption is. Set it to "self" for the A-Tag itself or use a callback which returns the element
+  captionType: 'attr', //'attr'	string	how to get the caption. You can choose between attr, data or text
+  captionsData: 'alt', //title	string	get the caption from given attribute
+  captionPosition: 'bottom', //'bottom'	string	the position of the caption. Options are top, bottom or outside (note that outside can be outside the visible viewport!)
+  captionDelay: 250, //int	adds a delay before the caption shows (in ms)
+});
+
 function inputRequest(request) {
   restGalleryService.query = request;
   restGalleryService.fetchArticles().then(insertMarkup);
+  lightbox.refresh();
 }
 
 // функция проверки ответа на наличие ошибки 404 'Page Not Found'
